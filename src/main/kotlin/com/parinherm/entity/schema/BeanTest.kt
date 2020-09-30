2d9295c82fd7d7bc4c33fd1e04aea1095cd188bb
paul martin <mussym@live.com>
2020-09-30 13:43:33 -0500

    database stuff

--- src/main/kotlin/com/parinherm/entity/schema/BeanTest.kt
+++ src/main/kotlin/com/parinherm/entity/schema/BeanTest.kt
@@ -1,8 +1,14 @@
 package com.parinherm.entity.schema
 
 import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.`java-time`.date
 
 object BeanTest: LongIdTable() {
     val name = varchar("name", 255)
-
+    val income = decimal("income", 10, 2)
+    val height = double("height")
+    val age = integer("age")
+    val country = varchar("country", 60)
+    val deceased = bool("deceased")
+    val enteredDate = date("enteredDate")
 }
