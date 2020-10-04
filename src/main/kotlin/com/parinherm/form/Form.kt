e36e0c431b64bfc94d27b5481e1af19c2cffb643
paul martin <mussym@live.com>
2020-10-04 08:30:41 -0500

    attempting a more functional immutable ui style

--- src/main/kotlin/com/parinherm/form/Form.kt
+++ src/main/kotlin/com/parinherm/form/Form.kt
@@ -1,10 +1,12 @@
 package com.parinherm.form
 
 import com.parinherm.ApplicationData
+import org.eclipse.jface.layout.GridDataFactory
 import org.eclipse.jface.layout.TableColumnLayout
 import org.eclipse.swt.SWT
 import org.eclipse.swt.custom.SashForm
 import org.eclipse.swt.layout.FillLayout
+import org.eclipse.swt.layout.GridLayout
 import org.eclipse.swt.widgets.Composite
 import org.eclipse.swt.widgets.Label
 
@@ -13,18 +15,25 @@ data class Form (val parent: Composite, val viewDefinition: Map<String, Any>) {
 
     val fields = viewDefinition[ApplicationData.ViewDef.fields] as List<Map<String, Any>>
     val composite = Composite(parent, ApplicationData.swnone)
-    val sashForm = SashForm(composite, SWT.BORDER)
+    val sashForm = SashForm(composite, SWT.BORDER or SWT.VERTICAL)
     val listContainer = Composite(sashForm, ApplicationData.swnone)
     val tableLayout = TableColumnLayout(true)
     val listView = getListViewer(listContainer, tableLayout)
     val columns = makeColumns(listView, fields, tableLayout )
-    val editContainer = Composite(parent, ApplicationData.swnone)
+    val editContainer = Composite(sashForm, ApplicationData.swnone)
     val lblErrors = Label(editContainer, ApplicationData.labelStyle)
 
 
     init {
         lblErrors.text = "hello there"
-        composite.layout = FillLayout()
+        editContainer.layout = GridLayout(2, false)
+        sashForm.weights = intArrayOf(1, 2)
+        sashForm.sashWidth = 4
+
+        GridDataFactory.fillDefaults().span(2, 1).applyTo(lblErrors)
+        composite.layout = FillLayout(SWT.VERTICAL)
+        composite.layout()
+
     }
 
 }
