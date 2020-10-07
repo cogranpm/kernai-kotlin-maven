676ba91004b472aeebdc178029356d79ee8635dd
paul martin <mussym@live.com>
2020-10-06 23:08:29 -0500

    attempting a more functional immutable ui style

--- src/main/kotlin/com/parinherm/viewmodel/PersonViewModel.kt
+++ src/main/kotlin/com/parinherm/viewmodel/PersonViewModel.kt
@@ -6,11 +6,10 @@ import com.parinherm.form.FormViewModel
 import com.parinherm.view.View
 
 class PersonViewModel(view: View) : FormViewModel<Person>(view) {
+
     init {
 
     }
 
-    fun loadData() {
 
-    }
 }
