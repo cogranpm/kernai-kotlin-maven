fac36b6342a53a479b43133788cc50d9dd0a9ce1
Paul Martin <cogranpm@gmail.com>
2020-10-10 10:28:21 -0500

    cleaning up view models

--- src/main/kotlin/com/parinherm/view/PersonView.kt
+++ src/main/kotlin/com/parinherm/view/PersonView.kt
@@ -4,10 +4,6 @@ import com.parinherm.ApplicationData
 import com.parinherm.builders.BeansViewerComparator
 import com.parinherm.entity.Person
 import com.parinherm.form.Form
-import org.eclipse.core.databinding.observable.list.WritableList
-import org.eclipse.jface.viewers.StructuredSelection
-import org.eclipse.swt.SWT
-import org.eclipse.swt.widgets.Button
 import org.eclipse.swt.widgets.Composite
 
 class PersonView(val parent: Composite, comparator: BeansViewerComparator) : View <Person> {
