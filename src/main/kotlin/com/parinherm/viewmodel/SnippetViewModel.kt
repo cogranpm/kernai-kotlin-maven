dcb12f306345e19df5e47107e7741df964492c2e
Paul Martin <cogranpm@gmail.com>
2020-10-13 20:36:58 -0500

    snippets

--- src/main/kotlin/com/parinherm/viewmodel/SnippetViewModel.kt
+++ src/main/kotlin/com/parinherm/viewmodel/SnippetViewModel.kt
@@ -3,7 +3,6 @@ package com.parinherm.viewmodel
 import com.parinherm.ApplicationData
 import com.parinherm.builders.BeansViewerComparator
 import com.parinherm.builders.IViewerComparator
-import com.parinherm.entity.Recipe
 import com.parinherm.entity.Snippet
 import com.parinherm.entity.schema.SnippetMapper
 import com.parinherm.form.FormViewModel
@@ -11,12 +10,31 @@ import com.parinherm.view.SnippetView
 import org.eclipse.jface.viewers.Viewer
 import org.eclipse.swt.custom.CTabFolder
 
+
+//import org.jetbrains.kotlin.config.KotlinCompilerVersion
+//import org.jetbrains.kotlin.daemon.common.threadCpuTime
+//import org.jetbrains.kotlin.cli.common.environment.setIdeaIoUseFallback
+import javax.script.*
+
 class SnippetViewModel (parent: CTabFolder)  : FormViewModel<Snippet>(
         SnippetView(parent, SnippetViewModel.Comparator()),
         SnippetMapper, { Snippet.make() }) {
 
     init {
         loadData(mapOf())
+        try {
+            val factory = ScriptEngineManager().getEngineByExtension("kts").factory
+            println(factory.engineName)
+            //val engine = factory!!.scriptEngine
+            //val bindings = engine.createBindings()
+            //val res1 = engine.eval("val x = 3")
+            val engine = ScriptEngineManager().getEngineByExtension("kts")!!
+            //val res1 = engine.eval("val x = 3")
+
+            //println(res1)
+        } catch(e: Exception) {
+            println(e)
+        }
     }
 
     class Comparator : BeansViewerComparator(), IViewerComparator {
