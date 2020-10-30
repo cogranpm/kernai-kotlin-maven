112fc2a6ffb6b6a73eb44b3e6862a2c36e634859
Paul Martin <mussym@live.com>
2020-10-30 13:51:36 -0500

    ui work

--- src/main/kotlin/com/parinherm/entity/schema/LookupDetailMapper.kt
+++ src/main/kotlin/com/parinherm/entity/schema/LookupDetailMapper.kt
@@ -2,23 +2,37 @@ package com.parinherm.entity.schema
 
 import com.parinherm.entity.Lookup
 import com.parinherm.entity.LookupDetail
-import com.parinherm.entity.Person
+import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.statements.UpdateBuilder
 
 object LookupDetailMapper: IMapper<LookupDetail> {
 
    val table = LookupDetails
 
+
+    fun mapItem(item: LookupDetail, statement: UpdateBuilder<Int>) : Unit {
+        statement[table.code] = item.code
+        statement[table.label] = item.label
+        statement[table.lookupId] = item.lookupId
+    }
+
     override fun save(item: LookupDetail) {
-        TODO("Not yet implemented")
+        MapperHelper.save(item, table, ::mapItem)
     }
 
     override fun getAll(keys: Map<String, Long>): List<LookupDetail> {
-        TODO("Not yet implemented")
+        return MapperHelper.getAll(keys, table, null, table.label to SortOrder.ASC) {
+            LookupDetail(
+                    it[table.id].value,
+                    it[table.lookupId],
+                    it[table.code],
+                    it[table.label])
+        }
     }
 
     override fun delete(item: LookupDetail) {
-        TODO("Not yet implemented")
+        MapperHelper.delete(table, table.id eq item.id)
     }
 
     fun delete(parent: Lookup) {
