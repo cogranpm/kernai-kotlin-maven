f59581fd275316d08f3649baac7cdd76d2586240
Paul Martin <cogranpm@gmail.com>
2020-11-14 19:45:03 -0600

    cleainging up a bit

--- JoyOfKotlin.ws.kts
+++ JoyOfKotlin.ws.kts
@@ -1,3 +1,5 @@
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
 import java.time.Instant
 
 // explicit constructor
@@ -29,11 +31,43 @@ class Judge private constructor(val name: String)
 // destructuring data objects
 data class Chef(val name: String, val registered: Instant = Instant.now())
 
+// each data class functions component1 to componentN are
+// automatically defined accessing properties in order they are
+// defined in the class
 
 fun show(chefs: List<Chef>) {
+
+    // desctructuring makes code clearer and less verbose
     for((name, date) in chefs){
         println("Name: $name Date: date")
     }
 }
 
 show(listOf(Chef("Val"), Chef("Piedro")))
+
+// companion object is designed for static members
+data class Fitter(val name: String, val registered: Instant = Instant.now()) {
+
+    companion object {
+        fun create(xml: String): Fitter {
+            println("do it mon")
+        }
+    }
+}
+
+// to call static member
+Fitter.create("blah blah")
+
+//objects: ie singletons
+//cannot have constructors
+object MyWindowAdapter: WindowAdapter() {
+    override fun windowClosed(e: WindowEvent?) {
+        super.windowClosed(e)
+    }
+}
+
+// collections
+// listOf is a package level function
+val readOnlyChefs = listOf(Chef("fred"))
+val mutChefs = mutableListOf(Chef("nigel"))
+
