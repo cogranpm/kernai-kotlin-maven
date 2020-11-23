498fbb8e2a251af089aa4bf04c1a24c9b1632ab2
Paul Martin <cogranpm@gmail.com>
2020-11-23 16:30:44 -0600

    implement filtering

--- src/main/kotlin/com/parinherm/model/TemplateHelpers.kt
+++ src/main/kotlin/com/parinherm/model/TemplateHelpers.kt
@@ -1,10 +1,11 @@
 package com.parinherm.model
 
 import com.parinherm.form.definitions.DataTypeDef
+import com.parinherm.form.definitions.FieldDef
 
 class TemplateHelpers {
-    fun translateDataTypeDef(dataTypeDef: DataTypeDef) : String =
-        when(dataTypeDef) {
+    fun translateDataTypeDef(dataTypeDef: DataTypeDef): String =
+        when (dataTypeDef) {
             DataTypeDef.MEMO, DataTypeDef.SOURCE, DataTypeDef.TEXT, DataTypeDef.LOOKUP -> "String"
             DataTypeDef.FLOAT -> "Double"
             DataTypeDef.MONEY -> "BigDecimal"
@@ -12,4 +13,35 @@ class TemplateHelpers {
             DataTypeDef.DATETIME -> "LocalDate"
             DataTypeDef.BOOLEAN -> "Boolean"
         }
+
+
+    fun translateColumnValue(fieldDef: FieldDef): String =
+        when (fieldDef.dataTypeDef) {
+            DataTypeDef.MEMO,
+            DataTypeDef.SOURCE,
+            DataTypeDef.TEXT,
+            DataTypeDef.FLOAT,
+            DataTypeDef.MONEY,
+            DataTypeDef.INT,
+            DataTypeDef.DATETIME,
+            DataTypeDef.BOOLEAN -> fieldDef.name
+            DataTypeDef.LOOKUP -> """ {
+                val listItem = ApplicationData.${fieldDef.lookupKey}.find { it.code == ${fieldDef.name}}
+                "$\{listItem?.label\}"
+            }"""
+        }
+
+    fun translateDefaultValue(fieldDef: FieldDef): String =
+        when (fieldDef.dataTypeDef) {
+            DataTypeDef.MEMO,
+            DataTypeDef.SOURCE,
+            DataTypeDef.TEXT -> "\"\""
+            DataTypeDef.LOOKUP -> "ApplicationData.${fieldDef.lookupKey}[0].code"
+            DataTypeDef.FLOAT -> "0.0"
+            DataTypeDef.MONEY -> "BigDecimal(0.0)"
+            DataTypeDef.INT -> "0"
+            DataTypeDef.DATETIME -> "LocalDate.now()"
+            DataTypeDef.BOOLEAN -> "False"
+        }
+
 }
