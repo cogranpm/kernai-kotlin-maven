a8e31005c5cc1627958e1ec1dc6de84cd51a1526
Paul Martin <cogranpm@gmail.com>
2020-11-25 14:46:41 -0600

    adding entity definitions

--- src/main/kotlin/com/parinherm/model/utils.kt
+++ src/main/kotlin/com/parinherm/model/utils.kt
@@ -1,9 +1,15 @@
 package com.parinherm.model
 
 import com.github.jknack.handlebars.Handlebars
+import com.github.jknack.handlebars.Template
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader
 import com.parinherm.form.definitions.ViewDef
 import org.stringtemplate.v4.*
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+
+val tempOutputDirectory = System.getProperty("user.home")
 
 
 
@@ -19,7 +25,16 @@ fun testHbars(viewDef: ViewDef){
     loader.prefix = "/templates"
     val hbars = Handlebars(loader)
     hbars.registerHelpers(TemplateHelpers())
-    val template = hbars.compile("models")
-    println(template.apply(viewDef))
+    val template = hbars.compile("entity")
+    writeTemplate(viewDef, template, viewDef.entityDef.name + ".kt", "entity")
 
+    val schema = hbars.compile("schema")
+    writeTemplate(viewDef, schema,viewDef.entityDef.name + "Schema.kt", "entity", "schema")
+}
+
+fun writeTemplate(viewDef: ViewDef, template: Template, fileName: String, vararg folders: String ) {
+    val tempOutputPath = Paths.get(tempOutputDirectory, "models", *folders)
+    Files.createDirectories(tempOutputPath)
+    val filePath = tempOutputPath.resolve(fileName)
+    File(filePath.toString()).writeText(template.apply(viewDef))
 }
